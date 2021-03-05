3ed22b3e793fa531eb4d9f0ae5f9fa6701a51f0a
BrianBell422 <brianbell.uk@gmail.com>
2021-03-05 16:36:05 +0000

    Added letter grid

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,15 +2,22 @@
   <section>
     <h1>Countdown Time!</h1>
     <timer/>
+    <letters-board/>
   </section>
 </template>
 
 <script>
 import Timer from '@/components/Timer.vue'
+import LetterBoard from '@/components/LetterBoard.vue'
+import LetterInput from '@/components/LetterInput.vue'
+import SubmitAnswers from '@/components/SubmitAnswers.vue'
   export default {
     
     components:{
-      'timer': Timer
+      'timer': Timer,
+      'letters-board': LetterBoard,
+      'letter-input': LetterInput,
+      'submit-answers': SubmitAnswers
     }
   }
 </script>
--- client/src/components/LetterBoard.vue
+++ client/src/components/LetterBoard.vue
@@ -1,15 +1,46 @@
 <template>
-    <section>
-    
+    <section >
+        <div id="letter-grid">
+            <p id="first-letter">A</p>
+            <p id="second-letter"></p>
+            <p id="third-letter"></p>
+            <p id="fourth-letter"></p>
+            <p id="fifth-letter"></p>
+            <p id="sixth-letter"></p>
+            <p id="seventh-letter"></p>
+            <p id="eighth-letter"></p>
+            <p id="ninth-letter"></p>
+        </div>
+
   </section>
 </template>
 
 <script>
     export default {
-        
+        data(){
+            return {
+                selected: true
+            }
+        }
     }
 </script>
 
 <style lang="css" scoped>
+
+#letter-grid{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+#letter-grid> p{
+    margin: 6px;
+    background-color: blue;
+    color: white;
+    padding:8px;
+    border-radius: 4px;
+}
+
+
 
 </style>
