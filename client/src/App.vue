09bebfe23bfeb28ab4de29af4e5074ee17719931
BrianBell422 <brianbell.uk@gmail.com>
2021-03-05 15:51:37 +0000

    Timer feature added and working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <section>
     <h1>Countdown Time!</h1>
-
+    <timer/>
   </section>
 </template>
 
 <script>
+import Timer from '@/components/Timer.vue'
   export default {
     
+    components:{
+      'timer': Timer
+    }
   }
 </script>
 
--- client/src/components/Timer.vue
+++ client/src/components/Timer.vue
@@ -1,15 +1,35 @@
 <template>
     <section>
-    
+        <h1 id="timer"></h1>
+        <button @click="startTimer">Start</button>
   </section>
 </template>
 
 <script>
     export default {
-        
+        name: 'timer',
+        data(){
+            return{
+            }
+        },
+        methods: {
+            startTimer(){
+                let countdownTimer = 4
+                let countdownTimerFunction = setInterval(function(){
+                    document.getElementById('timer').textContent='00:'+countdownTimer;
+                    countdownTimer--;
+                    if (countdownTimer < 0) {
+                        clearInterval(countdownTimerFunction);
+                        document.getElementById('timer').textContent = 'Time up'
+                    }
+                }, 1000);
+            }
+        }
     }
 </script>
 
 <style lang="css" scoped>
 
 </style>
+
+
