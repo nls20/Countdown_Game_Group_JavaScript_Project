e2925d6ab618a21708c7b2bbc768907f54f176cd
BrianBell422 <brianbell.uk@gmail.com>
2021-03-09 15:26:27 +0000

    started numbers submit answers page

---
+++ client/src/components/Numbers/SubmitAnswers.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <form @submit.prevent="submitNumbers">
+            <label for="playerOne">Player One: </label>
+            <input type="number" name="playerOne" v-model="playerOneNumber">
+
+            <label for="playerTwo">Player Two: </label>
+            <input type="number" name="playerTwo" v-model="playerTwoNumber" >
+
+            <input  type="submit" value="Submit Answer">
+      </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                playerOneNumber: 0,
+                playerTwoNumber: 0
+            }
+        },
+        methods:{
+            submitNumbers(){
+
+            }
+        }
+        
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
