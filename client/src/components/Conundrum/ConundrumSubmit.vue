d0e1ff8f36d5a0c4f3078a59bdc3df792e6c4137
Andrew Hunter <andrewhunter1612@gmail.com>
2021-03-09 20:48:40 +0000

    choose numbers css sorted

--- client/src/components/Conundrum/ConundrumSubmit.vue
+++ client/src/components/Conundrum/ConundrumSubmit.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-      <button>Stop Timer</button>
-      <form @submit.prevent="submitWords">
-            <label for="playerOne">Player One: </label>
-            <input type="text" name="playerOne" v-model="playerOneWord">
+        <button id="stop-button">Stop Timer</button>
+        <form id="player-input-form" @submit.prevent="submitWords">
+                <div class="player-input">
+                    <label for="playerOne">Player One: </label>
+                    <input type="text" name="playerOne" v-model="playerOneWord">
+                </div>
+            
+                <div class="player-input">  
+                    <label for="playerTwo">Player Two: </label>
+                    <input type="text" name="playerTwo" v-model="playerTwoWord" >
+                </div>
 
-            <label for="playerTwo">Player Two: </label>
-            <input type="text" name="playerTwo" v-model="playerTwoWord" >
-
-            <input  type="submit" value="Submit Answer">
-      </form>
+                <input id="submit-button" type="submit" value="Submit Answer">
+        </form>
   </div>
 </template>
 
@@ -43,6 +47,48 @@ export default {
 }
 </script>
 
-<style>
+<style lang="css" scoped>
+
+
+
+.player-input{
+  display: grid;
+  justify-content: center;
+  margin: 0px 30px;
+}
+
+.player-input>label{
+  text-align: center;
+  margin-bottom: 5px;
+}
+
+.player-input>input{
+  font-size: x-large
+}
+
+#player-input-form{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+#submit-button{
+  box-shadow:inset 0px 1px 0px 0px #97c4fe;
+  background:linear-gradient(to bottom, #3d94f6 5%, #1e62d0 100%);
+  background-color:#3d94f6;
+  border-radius:6px;
+  border:1px solid #337fed;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:large;
+  font-weight:bold;
+  padding:6px 24px;
+  text-decoration:none;
+  text-shadow:0px 1px 0px #1570cd;
+  margin: 20px 40px;
+  width: 180px;
+  height: 100px
+}
 
 </style>
--- client/src/components/Numbers/ChooseNumbers.vue
+++ client/src/components/Numbers/ChooseNumbers.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <h3>Choose number of large numbers</h3>
-        <div>
-            <button @click="setLargeNumber(0)">0</button>
-            <button @click="setLargeNumber(1)">1</button>
-            <button @click="setLargeNumber(2)">2</button>
-            <button @click="setLargeNumber(3)">3</button>
-            <button @click="setLargeNumber(4)">4</button>
+        <div id="choose-numbers">
+            <button class="large-button" @click="setLargeNumber(0)">0</button>
+            <button class="large-button" @click="setLargeNumber(1)">1</button>
+            <button class="large-button" @click="setLargeNumber(2)">2</button>
+            <button class="large-button" @click="setLargeNumber(3)">3</button>
+            <button class="large-button" @click="setLargeNumber(4)">4</button>
         </div>
     </div>
 </template>
@@ -29,7 +29,12 @@ import {eventBus} from '@/main.js'
 
 <style lang="css" scoped>
 
-button {
+#choose-numbers{
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.large-button {
     box-shadow:inset 0px 1px 0px 0px #97c4fe;
 	background:linear-gradient(to bottom, #3d94f6 5%, #1e62d0 100%);
 	background-color:#3d94f6;
@@ -44,9 +49,9 @@ button {
 	padding:6px 24px;
 	text-decoration:none;
 	text-shadow:0px 1px 0px #1570cd;
-    margin: 20px 40px;
-    width: 180px;
-    height: 100px
+    margin: 5px 10px;
+    width: 70px;
+    height: 40px
 }
 
 </style>
