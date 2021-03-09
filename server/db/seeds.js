8bf785ddc542ab8636c81d6de6bc2570834fceb7
BrianBell422 <brianbell.uk@gmail.com>
2021-03-09 11:38:20 +0000

    created conundrum db

---
+++ server/db/seeds.js
@@ -0,0 +1,37 @@
+use conundrum;
+
+db.dropDatabase()
+
+db.conundrumWords.insertMany([
+    {name: "Fireboard"},
+    {name: "Identical"},
+    {name: "Chocolate"},
+    {name: "Christmas"},
+    {name: "Beautiful"},
+    {name: "Happiness"},
+    {name: "Wednesday"},
+    {name: "Challenge"},
+    {name: "Celebrate"},
+    {name: "Adventure"},
+    {name: "Important"},
+    {name: "Consonant"},
+    {name: "Christian"},
+    {name: "Dangerous"},
+    {name: "Masculine"},
+    {name: "Australia"},
+    {name: "Irregular"},
+    {name: "Something"},
+    {name: "Knowledge"},
+    {name: "Elizabeth"},
+    {name: "Macaronic"},
+    {name: "Pollution"},
+    {name: "President"},
+    {name: "Wrestling"},
+    {name: "Pineapple"},
+    {name: "Adjective"},
+    {name: "Secretary"},
+    {name: "Undefined"},
+    {name: "Halloween"},
+])
+
+
