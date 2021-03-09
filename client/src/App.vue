2c0306508d1ee0a9973c92e6216a3666f70869be
BrianBell422 <brianbell.uk@gmail.com>
2021-03-09 14:12:45 +0000

    added numbers roung

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,22 +2,24 @@
   <section >
     <letter-round v-if="roundNumber === 1" />
     <conundrum v-if="roundNumber === 2" />
+    <number-round v-if="roundNumber === 3" />
   </section>
 </template>
 
 <script>
 import LetterRound from '@/components/LettersRound.vue'
 import ConundrumRound from '@/components/ConundrumRound.vue'
-
+import NumbersRound from '@/components/NumbersRound.vue'
   export default {
     data(){
       return {
-        roundNumber: 2
+        roundNumber: 3
       }
     },
     components:{
       'letter-round': LetterRound,
-      'conundrum': ConundrumRound
+      'conundrum': ConundrumRound,
+      'number-round': NumbersRound
     }
   }
 
